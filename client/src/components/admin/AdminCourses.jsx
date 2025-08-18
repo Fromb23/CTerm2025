fe4b8306396fba86a1f777884c5001b58765131d
Fromb23 <rombo.programming@gmail.com>
2025-08-18 12:18:44 -0400

    Refactor - Import CourseFormModal from the right path

--- client/src/components/admin/AdminCourses.jsx
+++ client/src/components/admin/AdminCourses.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Button from "../ui/Button";
 import Table from "../Table";
-import CourseFormModal from "./CourseFormModal";
+import CourseFormModal from "../CourseFormModal";
 
 // ------------------------ AdminCourses ------------------------
 const AdminCourses = () => {
