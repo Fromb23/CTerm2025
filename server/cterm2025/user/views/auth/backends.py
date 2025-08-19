7eabe50b796bbac774ea56c24257d989cef4fb8f
Fromb23 <rombo.programming@gmail.com>
2025-08-19 14:16:25 -0400

    chore(urls): include auth routes at root level
    - Updated project  to include  at the root path
    - Ensures authentication endpoints are accessible directly without prefix

---
+++ server/cterm2025/user/views/auth/backends.py
@@ -0,0 +1,19 @@
+# user/views/auth/backends.py
+from django.contrib.auth.backends import BaseBackend
+from user.models import CustomUser
+
+class EmailBackend(BaseBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            user = CustomUser.objects.get(email=username)
+        except CustomUser.DoesNotExist:
+            return None
+        if user.check_password(password):
+            return user
+        return None
+
+    def get_user(self, user_id):
+        try:
+            return CustomUser.objects.get(pk=user_id)
+        except CustomUser.DoesNotExist:
+            return None
