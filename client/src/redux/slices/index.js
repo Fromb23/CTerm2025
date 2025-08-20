fea8c5683982cf6ae354f76ab05e6e2a7b28f8cb
Fromb23 <rombo.programming@gmail.com>
2025-08-20 15:27:46 -0400

    refactor(store): integrate reducers for login, courses, and sprints
    - Added loginReducer for handling authentication state
    - Added courseReducer for managing course-related data
    - Added sprintReducer for sprint tracking and state management
    - Updated index.js to register the new reducers in the store

--- client/src/redux/slices/index.js
+++ client/src/redux/slices/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from 'redux'
 import signupReducer from './signupSlice.js'
 import loginReducer from './loginSlice.js'
+import courseReducer from './courseSlice.js'
+import sprintReducer from './sprintSlice.js'
 
 const rootReducer = combineReducers({
   signup: signupReducer,
-  login: loginReducer
+  login: loginReducer,
+  courses: courseReducer,
+  sprints: sprintReducer,
 })
 
 export default rootReducer
